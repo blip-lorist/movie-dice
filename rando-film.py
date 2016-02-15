54acf5f089a49b633539bdd86f06c9ab25a8b51a
Loraine <loraine@lorainekv.com>
2016-02-14 18:58:36 -0800

    Return random film from list of class suggestions.

---
+++ rando-film.py
@@ -0,0 +1,34 @@
+from flask import Flask, jsonify
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+import urllib
+import csv
+import random
+
+app = Flask(__name__)
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+@app.route('/rando-film/')
+def hello_world():
+    
+    # download google spreadsheet 
+    film_url = os.environ['FILM_URL']
+    urllib.urlretrieve(film_url, 'films.csv')
+
+    recs = []
+    # randomly select a film
+    with open('films.csv') as film_csv:
+        reader = csv.reader(film_csv) 
+        next(reader, None) # skip header
+
+        for line in reader:
+            recs.append(line[0])
+
+    choice = random.choice(recs)
+    # send it back via json
+    return jsonify(title=choice) 
+
+if __name__ == '__main__':
+    app.run()
