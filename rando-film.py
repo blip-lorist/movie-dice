879f6857f002e40d697d1fb40af7f0dfa0398742
Loraine <loraine@lorainekv.com>
2016-02-14 19:03:58 -0800

    Getting ready for heroku deploy.

--- rando-film.py
+++ rando-film.py
@@ -31,4 +31,5 @@ def hello_world():
     return jsonify(title=choice) 
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
